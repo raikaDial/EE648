9de8454e11d555131230da0a8c3772c082db16db
Cody Gossel <cgossel@alaska.edu>
2017-04-28 14:05:05 -0800

    Created figure for the entire 7 segment circuit

--- Matlab/MakePlots.m
+++ Matlab/MakePlots.m
@@ -7,20 +7,38 @@ Y = Parse_NGSpice(file);
 hold off
 n = 4.5;
 
-plot(Y(:,1),Y(:,2) + 4 * n)
+plot(Y(:,1),Y(:,2) + 10 * n)
 text(4e-9, 4 * n + 4, 'A')
 hold on
 
-plot(Y(:,1),Y(:,3) + 3 * n)
+plot(Y(:,1),Y(:,3) + 9 * n)
 text(4e-9, 3 * n + 4, 'B')
 
-plot(Y(:,1),Y(:,4) + 2 * n)
+plot(Y(:,1),Y(:,4) + 8 * n)
 text(4e-9, 2 * n + 4, 'C')
 
-plot(Y(:,1),Y(:,5) + 1 * n)
+plot(Y(:,1),Y(:,5) + 7 * n)
 text(4e-9, 1 * n + 4, 'D')
 
-plot(Y(:,1),Y(:,6) + 0 * n)
+plot(Y(:,1),Y(:,6) + 6 * n)
+text(4e-9, 0 * n + 4, 'Y')
+
+plot(Y(:,1),Y(:,7) + 5 * n)
+text(4e-9, 0 * n + 4, 'Y')
+
+plot(Y(:,1),Y(:,8) + 4 * n)
+text(4e-9, 0 * n + 4, 'Y')
+
+plot(Y(:,1),Y(:,9) + 3 * n)
+text(4e-9, 0 * n + 4, 'Y')
+
+plot(Y(:,1),Y(:,10) + 2 * n)
+text(4e-9, 0 * n + 4, 'Y')
+
+plot(Y(:,1),Y(:,11) + 1 * n)
+text(4e-9, 0 * n + 4, 'Y')
+
+plot(Y(:,1),Y(:,12) + 0 * n)
 text(4e-9, 0 * n + 4, 'Y')
 
 grid on
@@ -30,7 +48,7 @@ tick = 0:20:320;
 set(gca,'XTick',tick*1e-9);
 set(gca,'XTickLabel',[]);
 
-axis([0 320e-9 -.5 5*n])
+axis([0 320e-9 -.5 11*n])
 
 
 
--- spice/MakePlots.m
+++ spice/MakePlots.m
@@ -7,30 +7,48 @@ Y = Parse_NGSpice(file);
 hold off
 n = 4.5;
 
-plot(Y(:,1),Y(:,2) + 4 * n)
-text(4e-9, 4 * n + 4, 'A')
+plot(Y(:,1),Y(:,2) + 10 * n)
+text(4e-9, 10 * n + 2, 'A')
 hold on
 
-plot(Y(:,1),Y(:,3) + 3 * n)
-text(4e-9, 3 * n + 4, 'B')
+plot(Y(:,1),Y(:,3) + 9 * n)
+text(4e-9, 9 * n + 2, 'B')
 
-plot(Y(:,1),Y(:,4) + 2 * n)
-text(4e-9, 2 * n + 4, 'C')
+plot(Y(:,1),Y(:,4) + 8 * n)
+text(4e-9, 8 * n + 2, 'C')
 
-plot(Y(:,1),Y(:,5) + 1 * n)
-text(4e-9, 1 * n + 4, 'D')
+plot(Y(:,1),Y(:,5) + 7 * n)
+text(4e-9, 7 * n + 2, 'D')
 
-plot(Y(:,1),Y(:,6) + 0 * n)
-text(4e-9, 0 * n + 4, 'Y')
+plot(Y(:,1),Y(:,6) + 6 * n)
+text(4e-9, 6 * n + 2, 'Seg A')
+
+plot(Y(:,1),Y(:,7) + 5 * n)
+text(4e-9, 5 * n + 2, 'Seg B')
+
+plot(Y(:,1),Y(:,8) + 4 * n)
+text(4e-9, 4 * n + 2, 'Seg C')
+
+plot(Y(:,1),Y(:,9) + 3 * n)
+text(4e-9, 3 * n + 2, 'Seg D')
+
+plot(Y(:,1),Y(:,10) + 2 * n)
+text(4e-9, 2 * n + 2, 'Seg E')
+
+plot(Y(:,1),Y(:,11) + 1 * n)
+text(4e-9, 1 * n + 2, 'Seg F')
+
+plot(Y(:,1),Y(:,12) + 0 * n)
+text(4e-9, 0 * n + 2, 'Seg G')
 
 grid on
 
-set(gca,'YTick',[]);
+%set(gca,'YTick',[]);
 tick = 0:20:320;
 set(gca,'XTick',tick*1e-9);
 set(gca,'XTickLabel',[]);
 
-axis([0 320e-9 -.5 5*n])
+axis([0 320e-9 -.5 11*n])
 
 
 
